e7c372d1e2a8738ba634894c67b8231322b28524
hector6921 <hector_6921@hotmail.com>
2021-11-03 11:54:16 -0400

    first commit

---
+++ videoElement.js
@@ -0,0 +1,55 @@
+
+
+
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+
+//move image
+function moveCursor(coordinates){
+    console.log(coordinates)
+    let cursor = document.getElementById("cursor"); 
+}
+
+function onResults(results) {
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(
+      results.image, 0, 0, canvasElement.width, canvasElement.height);
+  if (results.multiHandLandmarks) {
+    for (const landmarks of results.multiHandLandmarks) {
+      console.log(landmarks[7].x)
+      drawConnectors(canvasCtx, landmarks[7], HAND_CONNECTIONS,
+                     {color: '#00FF00', lineWidth: 5});
+      drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
+      //moveCursor(landmarks);
+    //   document.getElementById("cursor").style.top = `${landmarks[7].y * 1000}px`;
+    //   document.getElementById("cursor").style.left = `${landmarks[7].x * 1000}px`;
+      document.getElementById("cursor").style.transform = "scale(5)";
+    }
+  }
+  canvasCtx.restore();
+}
+
+const hands = new Hands({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+}});
+hands.setOptions({
+  maxNumHands: 2,
+  modelComplexity: 1,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+hands.onResults(onResults);
+console.log(onResults)
+
+
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await hands.send({image: videoElement});
+  },
+  width: 1280,
+  height: 720
+});
+camera.start();
