3ca32e3b9732ee219248cc306adc7bacbb0dd1b3
hector6921 <hector_6921@hotmail.com>
2021-11-04 09:36:01 -0400

    click is working with a range of 10px from index finger y coordinates

--- videoElement.js
+++ videoElement.js
@@ -18,13 +18,24 @@ function onResults(results) {
       results.image, 0, 0, canvasElement.width, canvasElement.height);
   if (results.multiHandLandmarks) {
     for (const landmarks of results.multiHandLandmarks) {
-      console.log(landmarks[7].x)
-      drawConnectors(canvasCtx, landmarks[7], HAND_CONNECTIONS,
+      console.log(landmarks[7].y * 1000, " index")
+      console.log(landmarks[11].y * 1000, " middle")
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
                      {color: '#00FF00', lineWidth: 5});
+      drawConnectors(canvasCtx, [landmarks[8],landmarks[12]], HAND_CONNECTIONS,
+            {color: '#ff00ff', lineWidth: 5});
       drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
       //moveCursor(landmarks);
-      document.getElementById("cursor").style.top = `${landmarks[7].y * 1000}px`;
-      document.getElementById("cursor").style.left = `${landmarks[7].x * 1000}px`;
+      document.getElementById("cursor").style.top = `${landmarks[8].y * 1000}px`;
+      document.getElementById("cursor").style.left = `${landmarks[8].x * 1000}px`;
+      if(landmarks[12].y*1000 > (landmarks[8].y*1000)-10 && landmarks[12].y*1000 < (landmarks[8].y*1000)+10){
+        document.getElementById("clickCheck").click()
+        console.log("click")
+      }else{
+        console.log(
+            "nothing yet"
+        )
+      }
      //document.getElementById("cursor").style.transform = "scale(5)";
     }
   }
@@ -53,3 +64,8 @@ const camera = new Camera(videoElement, {
   height: 720
 });
 camera.start();
+function changeColor(){
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.getElementById("clickCheck").style.backgroundColor = `#${randomColor}`;
+    console.log(randomColor)
+}
